d13b82415abef5bbd540a5ad54dda3b32da508ac
Brian Landvogt <brian.landvogt@my.liu.edu>
2018-02-18 22:59:05 -0500

    Added new background images and JS functionality to several pages

---
+++ js/about.js
@@ -0,0 +1,7 @@
+//Fades in content.
+(function (){
+    $('#edu').hide().fadeIn(2000);
+    $('#eng').hide().fadeIn(3000);
+    $('#dev').hide().fadeIn(4000);
+    $('#bio').hide().fadeIn(5000);
+}());
---
+++ js/experience.js
@@ -0,0 +1,6 @@
+//Fades in content
+(function (){
+$('#work').hide().fadeIn(2000);
+$('#inn').hide().fadeIn(3000);
+$('#lead').hide().fadeIn(4000);
+}());
